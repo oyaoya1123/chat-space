03b2b6743cc6ee2adcfd80d739a6ad32197bc429
oyaoya1123 <oyaror@yahoo.co.jp>
2019-08-27 01:13:21 +0900

    make a message function by  Asynchronous Communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,55 @@
+function scrollBottom(){
+  var position = $('.messages')[0].scrollHeight;
+  $('.messages').animate({
+    scrollTop: position
+  }, 500, 'swing');
+}
+$(function(){
+  function buildHTML(message){
+    if(message.image){
+      var img = `<img class="message__image" src=${message.image} />`
+    } else {
+      img = ``
+    }
+    var html = `<div class='message'>
+                  <div class='message__upper-info'>
+                    <p class='message__upper-info__talker'>
+                      ${message.name}
+                    </p>
+                    <p class='message__upper-info__date'>
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class='message__text'>
+                    ${message.content}
+                  </p>
+                  ${img}
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.input-box__text').val('')
+      $('.input-box__image__file').val('')
+      scrollBottom();
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function(data){
+      $('.submit-btn').prop('disabled', false);
+      alert('error');
+    })
+  });
+});
